598e3b139b052c87f5c9cefe73773f930354478b
okkim <rodudtnf18@naver.com>
2023-02-17 14:45:14 +0900

    asdfasdf

--- assets/js/fileupload.js
+++ assets/js/fileupload.js
@@ -9,10 +9,20 @@ function select_file(target){
     let $val = $this.val();
     let $file_box = $this.parents('.file_box');
     let $input_txt = $file_box.find('.f_input');
+
+    
+
     console.log($val);
     if($val){
         $input_txt.val($val);
         $file_box.addClass('hasFile');
+        
+
+        
+        let $idx = $file_box.data('val');
+        let $del_chk = $(`.del_chk${$idx}`);
+        console.log($idx);
+        $del_chk .val($idx);
     }
 }
 
@@ -21,18 +31,24 @@ function del_file(target){
     let $file_box = $this.parents('.file_box');
     let $input_file = $file_box.find('.f_hidden');
     let $input_txt = $file_box.find('.f_input');
+    
     $file_box.removeClass('hasFile');
     $input_file.val(null);
     $input_txt.val(null);
     console.log($input_file.val());
 
 
+    let $idx = $file_box.data('val');
+    let $del_chk = $(`.del_chk${$idx}`);
+    console.log($idx);
+    $del_chk .val($idx);
+
 }
 
 function add_form(max){
     let $file_set = `<div class="file_set">
         <div class="file_box">
-            <input type="file" class="f_hidden" onchange="select_file(this)">
+            <input type="file" name="files[]" class="f_hidden" onchange="select_file(this)">
             <input type="text" class="f_input" placeholder="파일을 선택해주세요." onclick=" open_file(this)" readonly>
             <button class="btn_del" onclick=" del_file(this)"><span class="blind">파일삭제</span></button>
         </div>
@@ -50,6 +66,14 @@ function add_form(max){
 function del_form(target){
     let $this = $(target);
     let $file_set = $this.parents('.file_set');
+    let $file_box = $file_set.find('.file_box');
+    
+    let $idx = $file_box.data('val');
+    let $del_chk = $(`.del_chk${$idx}`);
+    console.log($idx);
+    $del_chk .val($idx);
+
+
     $file_set.remove();
     
 }
