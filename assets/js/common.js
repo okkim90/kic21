eaffa3117427abd1b348b97994e76e4face30eed
okkim <rodudtnf18@naver.com>
2023-02-24 13:02:07 +0900

    asdfasdf

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,15 +1,22 @@
-const r_nav = $('.right_nav_area');
-const r_nav_w = $('.right_nav_wrap');
-$(window).on('load scroll',function(){
+
+
+
+
+if($('.right_nav_area').length){
+    let r_nav = $('.right_nav_area');
+    let r_nav_w = $('.right_nav_wrap');
+    $(window).on('load scroll',function(){
     
-    let top_pos = r_nav_w.offset().top; 
-    let win_pos = this.scrollY;
-    if(win_pos > top_pos){
-        r_nav.addClass('on');
-    }else{
-        r_nav.removeClass('on');
-    }
-});
+        let top_pos = r_nav_w.offset().top; 
+        let win_pos = this.scrollY;
+        if(win_pos > top_pos){
+            r_nav.addClass('on');
+        }else{
+            r_nav.removeClass('on');
+        }
+    });
+}
+
 
 
 function close_popup(target){
